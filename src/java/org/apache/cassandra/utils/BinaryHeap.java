1845e4c3cef32777b97a5f840cd1dbde9c7e5237
Branimir Lambov <branimir.lambov@datastax.com>
2024-08-20 18:31:41 +0300

    Adds TopKSelector and extracts a common BinaryHeap core

---
+++ src/java/org/apache/cassandra/utils/BinaryHeap.java
@@ -0,0 +1,281 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.utils;
+
+import java.util.Comparator;
+
+/**
+ * A base binary heap implementation with fixed size, supporting only operations that push
+ * data down in the heap (i.e. after the initial initialization of the heap from a collection
+ * of items, only top/smallest items can be modified (removed or replaced with larger)).
+ * <p>
+ * This class does not implement a priority queue (because a queue's purpose is to support
+ * adding and removing items), but works very well as a source of sorted entries e.g. for
+ * merging iterators, producing a sorted iterator from an unsorted list of items
+ * ({@link SortingIterator}) or selecting the top items from data of unbounded size
+ * ({@link TopKSelector}).
+ * <p>
+ * The implementation supports nulls among the source entries and relies on support for them
+ * to make some operations more efficient.
+ * <p>
+ * This class is not intended to be used as a priority queue and does not support adding elements
+ * to the set after the initial construction. If a priority queue is required, see
+ * {@link LucenePriorityQueue}.
+ */
+public abstract class BinaryHeap
+{
+    // Note: This class is tested via its descendants by SortingIteratorTest and TopKSelectorTest.
+
+    protected final Object[] heap;
+
+    /**
+     * Create a binary heap with the given array. The data must be heapified before being used.
+     */
+    public BinaryHeap(Object[] data)
+    {
+        this.heap = data;
+        // Note that we can't perform any preparation here because the subclass defining greaterThan may have not been
+        // initialized yet.
+    }
+
+    /**
+     * Create a binary heap for the given size with no data. Data must be inserted manually
+     * into {@link #heap} and {@link #heapify} must be applied before the class's retrieval
+     * methods are used (see {@link TopKSelector}).
+     */
+    protected BinaryHeap(int size)
+    {
+        this.heap = new Object[size];
+    }
+
+    /**
+     * Compare two objects and return true iff the first is greater.
+     * The method must treat nulls as greater than non-null objects.
+     */
+    protected abstract boolean greaterThan(Object a, Object b);
+
+    /**
+     * Turn the current list of items into a binary heap by using the initial heap construction
+     * of the heapsort algorithm with complexity O(heap.length).
+     */
+    protected void heapify()
+    {
+        heapifyUpTo(heap.length);
+    }
+
+    /**
+     * Turn the list of items until the given index into a binary heap by using the initial heap construction
+     * of the heapsort algorithm with complexity O(heap.length).
+     */
+    protected void heapifyUpTo(int size)
+    {
+        for (int i = size / 2 - 1; i >= 0; --i)
+            heapifyDownUpTo(heap[i], i, size);
+    }
+
+    protected boolean isEmpty()
+    {
+        return heap[0] == null;
+    }
+
+    /**
+     * Return the next element in the heap without advancing.
+     */
+    protected Object peek()
+    {
+        return heap[0];
+    }
+
+    /**
+     * Get and remove the next element in the heap.
+     * If the heap contains duplicates, they will be returned in an arbitrary order.
+     */
+    protected Object pop()
+    {
+        return replaceTop(null);
+    }
+
+    /**
+     * Get and replace the top item with a new one. The new must compare greater than
+     * or equal to the item being replaced.
+     */
+    protected Object replaceTop(Object newItem)
+    {
+        Object item = heap[0];
+        heapifyDown(newItem, 0);
+        return item;
+    }
+
+    /**
+     * Get the next element and skip over all items equal to it.
+     * Calling this instead of {@link #pop} results in deduplication of the list
+     * of entries.
+     */
+    protected Object popAndSkipEqual()
+    {
+        Object item = heap[0];
+        advanceBeyond(item, 0);
+        return item;
+    }
+
+    /**
+     * Recursively drop all elements in the subheap rooted at the given heapIndex that are not beyond the given
+     * targetKey, and restore the heap ordering on the way back from the recursion.
+     */
+    private void advanceBeyond(Object targetKey, int heapIndex)
+    {
+        if (heapIndex >= heap.length)
+            return;
+        if (greaterThan(heap[heapIndex], targetKey))
+            return;
+
+        int nextIndex = heapIndex * 2 + 1;
+        if (nextIndex >= heap.length)
+        {
+            heap[heapIndex] = null;
+        }
+        else
+        {
+            advanceBeyond(targetKey, nextIndex);
+            advanceBeyond(targetKey, nextIndex + 1);
+
+            heapifyDown(null, heapIndex);
+        }
+    }
+
+    /**
+     * Skip to the first element that is greater than or equal to the given key.
+     */
+    protected void advanceTo(Object targetKey)
+    {
+        advanceTo(targetKey, 0);
+    }
+
+    /**
+     * Recursively drop all elements in the subheap rooted at the given heapIndex that are before the given
+     * targetKey, and restore the heap ordering on the way back from the recursion.
+     */
+    private void advanceTo(Object targetKey, int heapIndex)
+    {
+        if (heapIndex >= heap.length)
+            return;
+        if (!greaterThan(targetKey, heap[heapIndex]))
+            return;
+
+        int nextIndex = heapIndex * 2 + 1;
+        if (nextIndex >= heap.length)
+        {
+            heap[heapIndex] = null;
+        }
+        else
+        {
+            advanceTo(targetKey, nextIndex);
+            advanceTo(targetKey, nextIndex + 1);
+
+            heapifyDown(null, heapIndex);
+        }
+    }
+
+    /**
+     * Push the given state down in the heap from the given index until it finds its proper place among
+     * the subheap rooted at that position.
+     */
+    private void heapifyDown(Object item, int index)
+    {
+        heapifyDownUpTo(item, index, heap.length);
+    }
+
+    /**
+     * Push the given state down in the heap from the given index until it finds its proper place among
+     * the subheap rooted at that position.
+     */
+    private void heapifyDownUpTo(Object item, int index, int size)
+    {
+        while (true)
+        {
+            int next = index * 2 + 1;
+            if (next >= size)
+                break;
+            // Select the smaller of the two children to push down to.
+            if (next + 1 < size && greaterThan(heap[next], heap[next + 1]))
+                ++next;
+            // If the child is greater or equal, the invariant has been restored.
+            if (!greaterThan(item, heap[next]))
+                break;
+            heap[index] = heap[next];
+            index = next;
+        }
+        heap[index] = item;
+    }
+
+    /**
+     * Sort the heap by repeatedly popping the top item and placing it at the end of the heap array.
+     * The result will contain the elements in the heap sorted in descending order.
+     * The heap must be heapified up to the size before calling this method.
+     */
+    protected void heapSortUpTo(int size)
+    {
+        // Sorting the ones from 1 will also make put the right value in heap[0]
+        heapSortBetween(1, size);
+    }
+
+    /**
+     * Partially sort the heap by repeatedly popping the top item and placing it at the end of the heap array,
+     * until the given start position is reached. This results in a partial sorting where the smallest items
+     * (according to the comparator) are placed at positions of the heap between start and size in descending order,
+     * and the items before that are left heapified.
+     * The heap must be heapified up to the size before calling this method.
+     * Used to fetch items after a certain offset in a top-k selection.
+     */
+    protected void heapSortBetween(int start, int size)
+    {
+        // Data must already be heapified up to that size, comparator must be reverse
+        for (int i = size - 1; i >= start; --i)
+        {
+            Object top = heap[0];
+            heapifyDownUpTo(heap[i], 0, i);
+            heap[i] = top;
+        }
+    }
+
+    /**
+     * A binary heap that uses a comparator to determine the order of elements, implementing the necessary handling
+     * of nulls.
+     */
+    public static class WithComparator<T> extends BinaryHeap
+    {
+        final Comparator<? super T> comparator;
+
+        public WithComparator(Comparator<? super T> comparator, Object[] data)
+        {
+            super(data);
+            this.comparator = comparator;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        protected boolean greaterThan(Object a, Object b)
+        {
+            // nulls are treated as greater than non-nulls to be placed at the end of the sequence
+            if (a == null || b == null)
+                return b != null;
+            return comparator.compare((T) a, (T) b) > 0;
+        }
+    }
+}
---
+++ test/unit/org/apache/cassandra/utils/TopKSelectorTest.java
@@ -0,0 +1,241 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.utils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.google.common.base.Predicates;
+
+import static org.junit.Assert.*;
+
+public class TopKSelectorTest {
+
+    private TopKSelector<Integer> selector;
+
+    @Before
+    public void setUp() {
+        selector = new TopKSelector<>(Integer::compareTo, 5);
+    }
+
+    @Test
+    public void testBasicFunctionality() {
+        List<Integer> data = Arrays.asList(10, 20, 5, 7, 1, 3, 9, 15, 25);
+
+        for (int num : data) {
+            selector.add(num);
+        }
+
+        List<Integer> topK = selector.get();
+
+        // Expecting the smallest 5 elements in sorted order
+        assertEquals(Arrays.asList(1, 3, 5, 7, 9), topK);
+    }
+
+    @Test
+    public void testTopKSmallerThanK() {
+        List<Integer> data = Arrays.asList(10, 20, 5);
+
+        for (int num : data) {
+            selector.add(num);
+        }
+
+        List<Integer> topK = selector.get();
+
+        // Expecting all elements in sorted order because the total number is less than k
+        assertEquals(Arrays.asList(5, 10, 20), topK);
+    }
+
+    @Test
+    public void testTopKWithDuplicates() {
+        List<Integer> data = Arrays.asList(10, 20, 5, 5, 7, 10, 1, 3, 15, 25);
+
+        for (int num : data) {
+            selector.add(num);
+        }
+
+        List<Integer> topK = selector.get();
+
+        // Expecting the smallest 5 elements in sorted order
+        assertEquals(Arrays.asList(1, 3, 5, 5, 7), topK);
+    }
+
+
+    @Test
+    public void testTopKWithNulls() {
+        List<Integer> data = Arrays.asList(10, null, 5, 5, 7, 10, 1, 3, null, 25);
+
+        for (Integer num : data) {
+            selector.add(num);
+        }
+
+        List<Integer> topK = selector.get();
+
+        // Expecting the smallest 5 elements in sorted order
+        assertEquals(Arrays.asList(1, 3, 5, 5, 7), topK);
+    }
+
+
+    @Test
+    public void testTopKWithNullsShort() {
+        List<Integer> data = Arrays.asList(10, null, 5, 7, null);
+
+        for (Integer num : data) {
+            selector.add(num);
+        }
+
+        List<Integer> topK = selector.get();
+
+        // Expecting the non-null elements in sorted order
+        assertEquals(Arrays.asList(5, 7, 10), topK);
+    }
+
+    @Test
+    public void testTopKWithNegativeNumbers() {
+        List<Integer> data = Arrays.asList(-10, -20, -5, -7, -1, -3, -9, -15, -25);
+
+        for (int num : data) {
+            selector.add(num);
+        }
+
+        List<Integer> topK = selector.get();
+
+        // Expecting the smallest (most negative) 5 elements in sorted order
+        assertEquals(Arrays.asList(-25, -20, -15, -10, -9), topK);
+    }
+
+    @Test
+    public void testEmptyInput() {
+        List<Integer> topK = selector.get();
+
+        // Expecting an empty list because no elements were added
+        assertEquals(Collections.emptyList(), topK);
+    }
+
+    @Test
+    public void testSingleElementInput() {
+        selector.add(42);
+
+        List<Integer> topK = selector.get();
+
+        // Expecting the single element in the list
+        assertEquals(Arrays.asList(42), topK);
+    }
+
+    @Test
+    public void testRandomizedInput() {
+        Random random = new Random();
+        int size = 1000;
+        int k = random.nextInt(20) + 1;
+
+        TopKSelector<Integer> randomSelector = new TopKSelector<>(Integer::compareTo, k);
+        List<Integer> all = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Integer newItem = random.nextInt(10000); // Random values between 0 and 9999
+            randomSelector.add(newItem);
+            all.add(newItem);
+        }
+
+        List<Integer> topK = randomSelector.get();
+        List<Integer> sortedTopK = all.stream().sorted().limit(k).collect(Collectors.toList());
+
+        // Ensure the top k elements are sorted
+        assertEquals(sortedTopK, topK);
+
+        // Ensure the size of the top k list is k
+        assertEquals(k, topK.size());
+    }
+
+    @Test
+    public void testRandomizedInputNulls() {
+        Random random = new Random();
+        int size = 1000;
+        int k = random.nextInt(20) + 1;
+
+        TopKSelector<Integer> randomSelector = new TopKSelector<>(Integer::compareTo, k);
+        List<Integer> all = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Integer newItem = random.nextDouble() < 0.05 ? null : random.nextInt(10000); // Random values between 0 and 9999
+            randomSelector.add(newItem);
+            all.add(newItem);
+        }
+
+        List<Integer> topK = randomSelector.get();
+        List<Integer> sortedTopK = all.stream().filter(Predicates.notNull()).sorted().limit(k).collect(Collectors.toList());
+
+        // Ensure the top k elements are sorted
+        assertEquals(sortedTopK, topK);
+
+        // Ensure the size of the top k list is k
+        assertEquals(k, topK.size());
+    }
+
+
+    @Test
+    public void testRandomizedInputTransformedSliced() {
+        Random random = new Random();
+        int size = 1000;
+        int offset = random.nextInt(20) + 1;
+        int k = random.nextInt(20);
+
+        TopKSelector<Integer> randomSelector = new TopKSelector<>(Integer::compareTo, k + offset);
+        List<Integer> all = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Integer newItem = random.nextInt(10000); // Random values between 0 and 9999
+            randomSelector.add(newItem);
+            all.add(newItem);
+        }
+
+        Function<Integer, Integer> transformer = x -> x + 5;
+        List<Integer> topK = randomSelector.getTransformedSlicedShared(transformer, offset);
+        List<Integer> sortedTopK = all.stream().sorted().map(transformer).skip(offset).limit(k).collect(Collectors.toList());
+
+        // Ensure the top k elements are sorted
+        assertEquals(sortedTopK, topK);
+
+        // Ensure the size of the top k list is k
+        assertEquals(k, topK.size());
+    }
+
+    @Test
+    public void testAddMoreThanKElements() {
+        for (int i = 0; i < 20; i++) {
+            selector.add(i);
+        }
+
+        List<Integer> topK = selector.get();
+
+        // Expecting the smallest 5 elements in sorted order
+        assertEquals(Arrays.asList(0, 1, 2, 3, 4), topK);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testZeroK() {
+        var selector = new TopKSelector<>(Integer::compareTo, 0);
+        selector.addAll(List.of(10, 20, 3, 4, 5));
+        assertEquals(List.of(), selector.get());
+    }
+}
