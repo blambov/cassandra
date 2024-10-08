cf6bae1d2b379135e065fcdc0042501c69cd70c3
Branimir Lambov <branimir.lambov@datastax.com>
2024-10-08 15:24:56 +0300

    Switch to ILifecycleTransaction in compaction code

--- test/unit/org/apache/cassandra/db/compaction/UnifiedCompactionStrategyTest.java
+++ test/unit/org/apache/cassandra/db/compaction/UnifiedCompactionStrategyTest.java
@@ -1469,7 +1469,7 @@ public class UnifiedCompactionStrategyTest extends BaseCompactionStrategyTest
         Collection<AbstractCompactionTask> tasks = strategy.getNextBackgroundTasks(FBUtilities.nowInSeconds());
 
         assertEquals(1, tasks.size());
-        Set<SSTableReader> compacting = tasks.iterator().next().transaction.getCompacting();
+        var compacting = realm.getCompactingSSTables();
         assertEquals(2, compacting.size());
         assertEquals(new HashSet<>(sstables1), compacting);
     }
